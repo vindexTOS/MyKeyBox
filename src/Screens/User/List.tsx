d0be4a787e5aa73420bd5d8890175866fa1918ca
vindexTOS <giorgikutateladze1998@gmail.com>
2024-05-16 18:16:57 +0400

    useQuery started on list

--- src/Screens/User/List.tsx
+++ src/Screens/User/List.tsx
@@ -1,7 +1,13 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-
+import { GetBoxesRequest } from "../../API/Getlist";
+import { useQuery } from "@tanstack/react-query";
 export default function List() {
+  // const query = useQuery({
+  //   queryKey: ["box-list"],
+  //   queryFn: GetBoxesRequest,
+  // });
+
   return (
     <View style={style.mainContainer}>
       <Text style={style.header}>My list</Text>
